17c21b0276d08f095b2c780ec790ab6a26340739
Adrian Pena <adrian.pena@dominos.com>
2015-11-21 19:26:18 -0500

    Created the Alarm Manager and the Alarm model

---
+++ app/src/main/java/com/zabador/motherspills/alarm/model/Alarm.java
@@ -0,0 +1,58 @@
+package com.zabador.motherspills.alarm.model;
+
+import java.util.Date;
+
+/**
+ * The Alarm model.
+ * Created on 11/21/15.
+ *
+ * @author Adrian Pena
+ */
+public class Alarm {
+    private String name;
+    private String content;
+    private Object recipient;
+    private Date date;
+
+    public Alarm() {
+    }
+
+    public Alarm(String name, String content, Object recipient, Date date) {
+        this.name = name;
+        this.content = content;
+        this.recipient = recipient;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Object getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Object recipient) {
+        this.recipient = recipient;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
