5bea9961c22c10b37e135d48db934ce392d3e6e3
schneis <zabador2000@gmail.com>
2015-11-21 19:17:59 -0500

    Added new alarm activity

--- app/src/main/java/com/zabador/motherspills/activities/AddNewAlarmActivity.java
+++ app/src/main/java/com/zabador/motherspills/activities/AddNewAlarmActivity.java
@@ -1,9 +1,35 @@
 package com.zabador.motherspills.activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import com.zabador.motherspills.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created on 11/21/15.
  *
  * @author Skye Schneider
  */
-public class AddNewAlarmActivity {
+public class AddNewAlarmActivity extends Activity {
+
+    @Bind(R.id.recipient_number)
+    EditText mRecipientNumber;
+    @Bind(R.id.text_messge)
+    EditText mTextMessage;
+    @Bind(R.id.new_alarm_name)
+    EditText mAlarmName;
+    @Bind(R.id.time_picker)
+    TimePicker mTimePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_alarm);
+        ButterKnife.bind(this);
+    }
 }
