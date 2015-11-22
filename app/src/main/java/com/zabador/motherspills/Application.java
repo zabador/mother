9cc663ebf36337bc0c1a49421d8dd6b3d49c76b7
Adrian Pena <adrian.pena@dominos.com>
2015-11-21 23:27:31 -0500

    Added application for the SugarORM implementation

---
+++ app/src/main/java/com/zabador/motherspills/Application.java
@@ -0,0 +1,13 @@
+package com.zabador.motherspills;
+
+import com.orm.SugarApp;
+
+/**
+ * Mothers Pills Application entry point.
+ * Created on 11/21/15.
+ *
+ * @author Adrian Pena
+ */
+public class Application extends SugarApp {
+
+}
