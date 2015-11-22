4992ef5700f5bf891d9619b780e31c6ad46e7bd2
schneis <zabador2000@gmail.com>
2015-11-21 20:41:10 -0500

    Create a new date from time picker

--- app/src/main/java/com/zabador/motherspills/activities/AddNewAlarmActivity.java
+++ app/src/main/java/com/zabador/motherspills/activities/AddNewAlarmActivity.java
@@ -2,10 +2,14 @@ package com.zabador.motherspills.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
 import com.zabador.motherspills.R;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +20,9 @@ import butterknife.ButterKnife;
  * @author Skye Schneider
  */
 public class AddNewAlarmActivity extends Activity {
+    private static final String TAG = AddNewAlarmActivity.class.getSimpleName();
+
+    private Date mDate;
 
     @Bind(R.id.recipient_number)
     EditText mRecipientNumber;
@@ -31,5 +38,30 @@ public class AddNewAlarmActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_alarm);
         ButterKnife.bind(this);
+        mTimePicker.setIs24HourView(true);
+
+        mTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Log.d(TAG, " hour = " + hourOfDay + " minute = " + minute);
+                mDate = new Date();
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, hourOfDay, minute);
+                mDate = calendar.getTime();
+                Calendar cal = Calendar.getInstance();
+                cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                cal.set(Calendar.MINUTE, minute);
+                mDate = cal.getTime();
+
+
+                boolean beforeNow = mDate.before(Calendar.getInstance().getTime());
+                if (beforeNow) {
+                    Log.d(TAG, "It was before");
+                    cal.add(Calendar.DATE, 1);
+                    mDate = cal.getTime();
+                }
+
+            }
+        });
     }
 }
